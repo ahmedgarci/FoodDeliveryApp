693af18d9852e448af4864e42e45f3dd56cc6153
ahmedgarci <124158017+ahmedgarci@users.noreply.github.com>
2024-10-25 16:13:35 +0200

    Update createJwtToken.js

--- Infrastructure/JsonWebToken/createJwtToken.js
+++ Infrastructure/JsonWebToken/createJwtToken.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-let SecretKey = "127Grnypteed277652YR4H";
+let SecretKey = "#";
 
 module.exports = (data,expiresAt)=>{
     return jwt.sign({data,expiresAt},SecretKey,{expiresIn:3600})
